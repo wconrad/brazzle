6310be6eae58498aac04d006a7b61904a77296ca
Wayne Conrad <wconrad@yagni.com>
2013-04-28 07:18:34 -0700

    Set segment registers

--- boot.asm
+++ boot.asm
@@ -1,8 +1,20 @@
-; HANG.ASM
-; A minimal bootstrap
+; JUMP.ASM
+; Make a jump and then hang
 
-hang:                   ; Hang!
+; Tell the compiler that this is offset 0.
+; It isn't offset 0, but it will be after the jump.
+[ORG 0]
+
+        jmp 07C0h:start         ; Goto segment 07C0
+
+start:
+        ; Update the segment registers
+        mov ax, cs
+        mov ds, ax
+        mov es, ax
+
+hang:                           ; Hang!
         jmp hang
 
-times 510-($-$$) db 0   ; Fill the file with 0's
-dw 0AA55h               ; End the file with AA55
+times 510-($-$$) db 0
+dw 0AA55h
