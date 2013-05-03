c3bbcca71f8edb1aab54471badaebac9d9607d3f
Wayne Conrad <wconrad@yagni.com>
2013-05-02 17:52:19 -0700

    boot sector uses LBA to load 2nd stage

--- boot.asm
+++ boot.asm
@@ -1,16 +1,19 @@
 ;;; Boot sector
 ;;; 
 ;;; * Load program starting at sector 1
-;;; * Switch to protected mode
 ;;; * Run it
 
-;;; Tell the compiler that this is offset 0.
-;;; It isn't offset 0, but it will be after the jump.
-
         bits    16
-        org     0x7c00
 
-        jmp     0:start         ;cs = 0
+stage2: equ     0x1000          ; Address of second stage loader
+stack:  equ     stage2          ; Stack grows down from here
+
+;;; The BIOS loads us at 0x00007c00, but with the segment register
+;;; nknown (it could be 0000:7c00, or 007c:0000, etc.)  Force
+;;; the segment register to 0.
+
+        org     0x7c00
+        jmp     0:start
 start:
 
 ;;; DS = ES = CS
@@ -26,7 +29,7 @@ start:
 ;;; Set up a stack
 
         cli
-        mov     ax,0x900
+        mov     ax,stack
         mov     ss,ax
         mov     sp,0
         sti
@@ -36,15 +39,11 @@ start:
         mov     si,bootmsg
         call    message
 
-;;; Read second stage loader from the drive.
+;;; Read second stage loader from the drive using LBA addressing mode.
 
-        mov     ah,2            ; read sectors from drive
-        mov     al,10           ; sector count
-        mov     ch,0            ; track
-        mov     cl,2            ; sector
-        mov     dh,0            ; head
-        mov     dl,[bootdrv]    ; drive
-        mov     bx,0x1000
+        mov     ah,0x42                 ; read sectors, LBA mode
+        mov     dl,[bootdrv]            ; drive
+        mov     si,dap                  ; disk address packet
         int     0x13
 
         mov     si,loaded_msg
@@ -52,7 +51,7 @@ start:
 
 ;;; Branch to it
 
-        jmp     0:0x1000
+        jmp     0:stage2
 
 ;;; Display NULL terminated string at ds:si
         
@@ -60,17 +59,37 @@ message:
         lodsb
         or      al,al
         jz      done
-        mov     ah,0x0e
-        mov     bx,7
-        int     0x10
+        call    bios_putc
         jmp     message
 done:
         ret
+
+;;; Call the BIOS to write AL to the screen, TTY style
         
+bios_putc:      
+        mov     ah,0x0e
+        mov     bx,7                    ; attribute
+        int     0x10
+        ret
 
 bootdrv:        db      0
 bootmsg:        db      'Loading',13,10,0
 loaded_msg:     db      'Loaded',13,10,0
 
+;;; The disk address packet describes what to read and where to store
+;;; it.
+
+dap:    
+        db      dap_size        ; size of packet
+        db      0               ; always 0
+        dw      10              ; number of sectors to transfer
+        dw      stage2          ; buffer offset
+        dw      0               ; buffer segment
+        dd      1               ; LBA (lower)
+        dd      0               ; LBA (upper)
+dap_size: equ $ - dap
+
         times 510-($-$$) db 0
-dw 0AA55h
+        dw 0AA55h
+
+        end
