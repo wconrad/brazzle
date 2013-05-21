b4aeaf99c621d17a147acf431d7e131c6860f5ac
Wayne Conrad <wayne@databill.com>
2013-05-21 12:52:23 -0700

    Tweak display of BIOS memory map

--- bmmap.c
+++ bmmap.c
@@ -9,14 +9,14 @@ void bmmap_print() {
   vty_printf("%d entries at %p\n",
              *bios_memmap_entries_ptr,
              bios_memmap_ptr);
-  vty_printf("ADDR LOW   ADDR HI   LEN LOW    LEN HI      TYPE  RESERVED\n");
+  vty_printf("       ADDR               LEN           TYPE    RESERVED\n");
   for(int i = 0; i < *bios_memmap_entries_ptr; i++) {
     BmmapEntry * entry = &(*bios_memmap_ptr)[i];
-    vty_printf("%08x  %08x  %08x  %08x  %08x  %08x\n",
-               entry->base_addr_low,
+    vty_printf("%08x:%08x  %08x:%08x  %08x  %08x\n",
                entry->base_addr_high,
-               entry->length_low,
+               entry->base_addr_low,
                entry->length_high,
+               entry->length_low,
                entry->type,
                entry->reserved);
   }
